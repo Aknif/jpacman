fcdf7a602db4109374b74477d227d9624d94c977
NickKanyarat <kanyarat.yu@kkumail.com>
2023-03-18 21:26:05 +0700

    Add score background and back button

--- src/main/java/nl/tudelft/jpacman/ui/ScoreUI.java
+++ src/main/java/nl/tudelft/jpacman/ui/ScoreUI.java
@@ -14,9 +14,38 @@ public class ScoreUI extends JFrame {
     public ScoreUI() {
         // Initialize the ScoreUI screen
         setTitle("Score");
-        setSize(200, 100);
+        setSize(800, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        int frameWidth = getWidth();
+        int frameHeight = getHeight();
+        int buttonWidth = 116;
+        int buttonHeight = 51;
+        int buttonX = frameWidth - buttonWidth;
+        int buttonY = frameHeight - buttonHeight;
+
+        ImageIcon transparentIcon = new ImageIcon("src/main/resources/Button 116, 51 px/back  116, 51 px.png");
+        JLabel backgroundLabel = new JLabel(new ImageIcon("src/main/resources/pacman_bg/Score_bg  800, 600 px.png"));
+
+        JButton BackButton = new JButton(transparentIcon);
+        BackButton.setBorderPainted(false);
+        BackButton.setContentAreaFilled(false);
+        BackButton.setFocusPainted(false);
+
+        BackButton.addActionListener(e -> {
+            HomeUI homeUI = new HomeUI();
+            dispose();
+        });
+
+        BackButton.setBounds(buttonX-640, buttonY-25, buttonWidth, buttonHeight);
+        backgroundLabel.add(BackButton);
+
+        add(backgroundLabel);
+
+        pack();
+        setVisible(true);
 
         // Initialize the score and add a JLabel to display it
         score = 0;
